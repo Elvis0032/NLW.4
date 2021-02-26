1a6d81f152509aac4dca2cd117b409bc519cf5f7
Antônio Elvis Pinheiro <antonioelvis.300@gmail.com>
2021-02-26 20:23:12 -0300

    Experiencia + Desafios
    Erro da barra de experiência corrigido.

--- moveit-next/src/components/ExperienceBar.tsx
+++ moveit-next/src/components/ExperienceBar.tsx
@@ -1,18 +1,19 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ExperienceBar.module.css';
 
 export function ExperienceBar(){
     const {currentExperience, experienceToNextLevel} = useContext(ChallengesContext);
 
-    const percentToNextLevel = Math.round(currentExperience *100)/experienceToNextLevel;
+    const percentToNextLevel = Math.round(currentExperience * 100)/experienceToNextLevel;
     return(
         <header className = {styles.experienceBar}> 
             <span>0 xp</span>
+
             <div>
                 <div style={{width:`${percentToNextLevel}%`}}/>
 
-                <span className={styles.currentExperience} style={{left:`${experienceToNextLevel}%`}}>
+                <span className={styles.currentExperience} style={{left:`${percentToNextLevel}%`}}>
                     {currentExperience} xp
                 </span>
             </div>
