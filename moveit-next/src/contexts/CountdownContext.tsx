265721e6781b1d6289e9286f952624a34d6013ba
Antônio Elvis Pinheiro <antonioelvis.300@gmail.com>
2021-02-28 15:06:46 -0300

    Erros corrigidos
    Timeout =:>>> problema corrigido

--- moveit-next/src/contexts/CountdownContext.tsx
+++ moveit-next/src/contexts/CountdownContext.tsx
@@ -3,11 +3,11 @@ import { ChallengesContext } from "./ChallengesContext";
 
 interface CountdownContextData {
     minutes: number;
-    seconds:number;
-    hasFinished:boolean;
-    isActive:boolean;
-    startCountdown:() => void;
-    resetCountdown:() => void;
+    seconds: number;
+    hasFinished: boolean;
+    isActive: boolean;
+    startCountdown: () => void;
+    resetCountdown: () => void;
 }
 
 interface CountdownProviderProps {
@@ -19,10 +19,10 @@ export const CountdownContext = createContext({} as CountdownContextData)
 
 let countdownTimeout: NodeJS.Timeout;
 
-export function CountdownProvider({children}: CountdownProviderProps) {
+export function CountdownProvider({ children }: CountdownProviderProps) {
     const { startNewChallenge } = useContext(ChallengesContext);
 
-    const [time, setTime] = useState(25 * 60);
+    const [time, setTime] = useState(0.1 * 60);
     const [isActive, setIsActive] = useState(false);
     const [hasFinished, setHasFinished] = useState(false);
 
@@ -32,13 +32,13 @@ export function CountdownProvider({children}: CountdownProviderProps) {
     function startCountdown() {
         setIsActive(true);
     }
-    
-    function resetCountdown(){
+
+    function resetCountdown() {
 
         clearTimeout(countdownTimeout);
         setIsActive(false);
         setHasFinished(false);
-        setTime(0.1*60);
+        setTime(0.1 * 60);
 
     }
 
@@ -47,10 +47,10 @@ export function CountdownProvider({children}: CountdownProviderProps) {
             countdownTimeout = setTimeout(() => {
                 setTime(time - 1);
             }, 1000)
-        }else if(isActive && time ==0){
-           setHasFinished(true);
-           setIsActive(false);
-           startNewChallenge();
+        } else if (isActive && time == 0) {
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
         }
     }, [isActive, time]);
 
@@ -62,7 +62,8 @@ export function CountdownProvider({children}: CountdownProviderProps) {
             hasFinished,
             isActive,
             startCountdown,
-            resetCountdown}}>
+            resetCountdown
+        }}>
             {children}
         </CountdownContext.Provider>
     )
